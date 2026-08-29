d966e411cc9f06b88f6e8f8aa93faccdd51572a5
helenypzhang <ypzhang5-c@my.cityu.edu.hk>
2026-08-29 11:22:37 +0100

    Use white publication image backgrounds

--- src/components/publications/PublicationsList.tsx
+++ src/components/publications/PublicationsList.tsx
@@ -215,7 +215,7 @@ export default function PublicationsList({ config, publications, embedded = fals
                             <div className="flex flex-col md:flex-row gap-6">
                                 {pub.preview && (
                                     <div className="w-full md:w-48 flex-shrink-0">
-                                        <div className="aspect-[16/10] relative rounded-lg overflow-hidden bg-neutral-100 dark:bg-neutral-800">
+                                        <div className="aspect-[16/10] relative rounded-lg overflow-hidden bg-white dark:bg-neutral-800 border border-neutral-100 dark:border-neutral-700">
                                             <Image
                                                 src={`/papers/${pub.preview}`}
                                                 alt={pub.title}
